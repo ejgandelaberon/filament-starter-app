da9693bd9dc64dfc738f1e3fdf24f769deaf92c2
Emmanuel Joseph Beron <emsephron@gmail.com>
2024-08-04 15:26:35 +0800

    change panel access condition

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\SystemRoleEnum;
-use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
 use Database\Factories\UserFactory;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Config;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
@@ -19,7 +20,6 @@ class User extends Authenticatable implements FilamentUser
     /** @phpstan-use HasFactory<UserFactory> */
     use HasFactory;
 
-    use HasPanelShield;
     use HasRoles;
     use Notifiable;
 
@@ -60,5 +60,10 @@ class User extends Authenticatable implements FilamentUser
     public function isSuperAdmin(): bool
     {
         return $this->hasRole(SystemRoleEnum::SUPER_ADMIN->value);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_contains($this->email, Config::string('app.domain'));
     }
 }
