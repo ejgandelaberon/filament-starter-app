0420b128de5101723b4394cafd103c58bde235f4
Emmanuel Joseph Beron <emsephron@gmail.com>
2024-08-04 12:59:18 +0800

    pint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Filament\FilamentConfigurations;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Columns\Column;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
