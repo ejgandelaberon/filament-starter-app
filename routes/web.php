0b4a2166b50c9c012df6d2f9e0d55e20b390b0a1
Emmanuel Joseph Beron <emsephron@gmail.com>
2024-08-12 21:40:10 +0800

    disable fortify and jetstream routes

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,7 @@
 
 declare(strict_types=1);
 
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/', fn () => redirect(Filament::getUrl()));
