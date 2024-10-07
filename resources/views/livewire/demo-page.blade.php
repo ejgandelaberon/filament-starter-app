52c5411867fe06a802424d0fd2a7a7564ad28b68
Emmanuel Joseph Beron <emsephron@gmail.com>
2024-10-07 18:05:08 +0800

    style: style dt container

--- resources/views/livewire/demo-page.blade.php
+++ resources/views/livewire/demo-page.blade.php
@@ -1,5 +1,5 @@
-<div>
-    <section class="mx-auto py-24 grid place-items-center w-screen">
+<section class="max-w-7xl mx-auto py-12">
+    <div class="border border-gray-200 shadow rounded-lg px-6 py-4">
         {{ $this->dataTable }}
-    </section>
-</div>
+    </div>
+</section>
