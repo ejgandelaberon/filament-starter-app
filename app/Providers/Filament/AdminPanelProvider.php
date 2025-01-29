433a8ce8c996a4393f6f23c6271cef3d963d8e24
Emmanuel Joseph Beron <emsephron@gmail.com>
2025-01-30 00:27:33 +0800

    style(Panel): set default font to Poppins

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,6 +51,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login(LoginPage::class)
             ->profile(ProfileInformation::class, isSimple: false)
+            ->font('Poppins')
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
